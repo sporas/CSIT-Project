3a380851ab5aa8075a342de6db86570dfd448ae3
sporas <ianrong1371@gmail.com>
2021-01-10 19:06:15 -0800

    Add files via upload

---
+++ API/ActualFile/TodoApi/TodoApi/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Filters
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        
+        private const string ApiKeyHeaderName = "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            if(!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = configuration.GetValue<string>(key: "ApiKey");
+
+            if(!apiKey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ API/ActualFile/TodoApi/TodoApi/Methods/CheckType.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+
+using DidiSoft.Pgp;
+
+namespace TodoApi.Methods
+{
+    public class CheckType
+    {
+
+        public int GetBase64FileSize (string base64string)
+        {
+            int length = base64string.Length;
+            int countequal = base64string.Count(x => x == '=');
+            int filesize = (int)(length * 0.75) - countequal;
+            return filesize;
+
+        }
+        public string GetFileExtension (string base64String)
+        {
+            var data = base64String.Substring(0, 5);
+
+            switch (data.ToUpper())
+            {
+                case "IVBOR":
+                    return "png"; 
+                case "/9J/4":
+                    return "jpg"; 
+                case "AAAAF":
+                    return "mp4"; 
+                case "JVBER":
+                    return "pdf"; 
+                case "U1PKC":
+                    return "txt";
+                case "UEsDB":
+                    return "docx";
+                
+                case "MQOWM":
+                case "77U/M":
+                    return "srt";
+                case "AAABA":
+                    return "ico";
+                case "UMFYI":
+                    return "rar";
+                case "E1XYD":
+                    return "rtf";
+                
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public string CreatePath (string key, string name, string classification, string filetype)
+        {
+            StringBuilder strWFBuilderCheck = new StringBuilder();
+            
+
+            strWFBuilderCheck.Append(@"C:\Pending\");
+            //item.Classification.ToString().ToLower()
+            strWFBuilderCheck.Append(classification);
+
+            if (classification == "restricted" || classification == "confidential" || classification == "secret")
+            { }
+            else
+            {
+                return "NO";
+            }
+
+
+            strWFBuilderCheck.Append("_");
+            //item.Name.ToString()
+            strWFBuilderCheck.Append(key);
+            strWFBuilderCheck.Append("_");
+            strWFBuilderCheck.Append(name);
+           
+
+            strWFBuilderCheck.Append(".");
+            //bb64type
+            strWFBuilderCheck.Append(filetype);
+            
+
+            return strWFBuilderCheck.ToString();
+            
+
+        }
+
+        public string CreateEncryptedPath(string key, string name, string classification, string filetype)
+        {
+            StringBuilder buildencryptedpath = new StringBuilder();
+            buildencryptedpath.Append(@"\\MURAKAMIPC\Pending_Decryption\");
+            // buildencryptedpath.Append(@"C:\Encrypted\");
+            //item.Classification.ToString().ToLower()
+            buildencryptedpath.Append(classification);
+            buildencryptedpath.Append("_");
+            //item.Name.ToString()
+            buildencryptedpath.Append(key);
+            buildencryptedpath.Append("_");
+            buildencryptedpath.Append(name);
+            buildencryptedpath.Append(".");
+            //bb64type
+            buildencryptedpath.Append(filetype);
+            buildencryptedpath.Append(".pgp");
+            return buildencryptedpath.ToString();
+            
+        }
+
+        public bool PGPEncrypt (string path, string encryptedpath)
+        {
+            PGPLib pgp = new PGPLib();
+            pgp.OverrideKeyAlgorithmPreferences = true;
+            pgp.Cypher = CypherAlgorithm.AES_256;
+            bool asciiArmor = false;
+            bool withIntegrityCheck = true;
+
+            try
+            {
+                pgp.EncryptFile(path,
+                                @"C:\Users\JosephHeller\Documents\Server_Key_0xF81974C9_public.asc",
+                                encryptedpath,
+                                asciiArmor,
+                                withIntegrityCheck);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+         
+        }
+    }
+}
