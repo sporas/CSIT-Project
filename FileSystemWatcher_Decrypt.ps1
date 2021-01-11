cdf1171ff5d177cab532fb36493ca0fbac477a5d
sporas <ianrong1371@gmail.com>
2021-01-10 18:30:41 -0800

    Update FileSystemWatcher_Decrypt.ps1

--- FileSystemWatcher_Decrypt.ps1
+++ FileSystemWatcher_Decrypt.ps1
@@ -18,6 +18,14 @@ $FileSystemWatcher.EnableRaisingEvents = $true
 # define the code that should execute when a file change is detected
 $Action = 
 {
+
+    $LogFile = "C:\Powerlogs\testing.txt"
+    Function LogWrite
+    {
+        Param ([string]$logstring)
+        Add-Content $LogFile -value $logstring
+    }
+
     $details = $event.SourceEventArgs
     $Name = $details.Name
     $FullPath = $details.FullPath
@@ -41,13 +49,67 @@ $Action =
     {
         'Changed' 
         {
-            gpg --pinentry-mode loopback --yes --output $outputpath --decrypt $FullPath
+            LogWrite ""
+            LogWrite "Changed Event Raised"
+
+            if ($BaseName.StartsWith("restricted_"))
+            {
+                $outputpath = "C:\Decrypted\Restricted\"    
+                Write-Host "Output path is set to Restricted folder"
+                LogWrite "Output path is set to Restricted folder"
+                $namewithoutclass = $BaseName.Substring(11)
+            }
+
+            if ($BaseName.StartsWith("confidential_"))
+            {
+                $outputpath = "C:\Decrypted\Confidential\"    
+                Write-Host "Output path is set to Confidential folder"
+                LogWrite "Output path is set to Confidential folder"
+                $namewithoutclass = $BaseName.Substring(13)
+            }
+
+             if ($BaseName.StartsWith("secret_"))
+            {
+                $outputpath = "C:\Decrypted\Secret\"     
+                Write-Host "Output path is set to Secret folder"
+                LogWrite "Output path is set to Secret folder"
+                $namewithoutclass = $BaseName.Substring(7)
+            }
+
+            $text456 = $namewithoutclass.Substring(0, 36)
+            Write-Host $text456 -ForegroundColor Yellow
+            LogWrite $text456
+
+
+
+            $namewithoutanything = $namewithoutclass.Substring(37)
+
+            $outputpathwithoutfolder = $outputpath + $namewithoutclass.Substring(0, 36) + "\"
+
+            $text123 = "Output path without folder: {0}" -f $outputpathwithoutfolder
+            New-Item -ItemType directory -Path $outputpathwithoutfolder
+            Write-Host $text123
+            LogWrite $text123
+
+
+            $outputpath = $outputpathwithoutfolder + $namewithoutanything
+
+            gpg --pinentry-mode loopback --yes --output $outputpath --decrypt $FullPath 2> C:\Powerlogs\gpgbugs.txt
             
             $text3 = "INPUT: {0}" -f $FullPath
             $text4 = "OUTPUT: {0}" -f $outputpath
 
             Write-Host $text3
             Write-Host $text4
+            LogWrite $text3
+            LogWrite $text4
+
+            $text = "File {0} was decrypted with GPG" -f $Name
+            Write-Host $text -ForegroundColor Yellow
+            LogWrite $text
+            $text1 = "File {0} has been sent to Decrypted folder. Renamed to {1}" -f $Name, $namewithoutanything
+            Write-Host $text1
+            LogWrite $text1
 
             Start-Sleep -s 15
 
@@ -59,15 +121,27 @@ $Action =
 
         'Created' 
         { 
+            LogWrite ""
+            LogWrite "Created Event Raised"
+
        
          #  gpg --pinentry-mode loopback --yes --output $outputpath --decrypt $FullPath
        #   gpg --output "C:\Decrypted\Approved_Interim Report.pdf" --decrypt "C:\Pending_Decryption\Approved_Interim Report.pdf.gpg" 
         
            # gpg --pinentry-mode=loopback --decrypt $FullPath --output $outputpath
+
+
+           
             $text = "File {0} was decrypted with GPG" -f $Name
             Write-Host $text -ForegroundColor Yellow
+            LogWrite $text
             $text1 = "File {0} has been sent to Decrypted folder. Renamed to {1}" -f $Name, $BaseName
             Write-Host $text1
+            LogWrite $text1
+
+
+
+
           #  Remove-Item -Path $Fullpath
            # Write-Host $Name "has been removed!" 
 
