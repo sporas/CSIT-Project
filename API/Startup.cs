a0e304effcba1f1a81e7318e7a82342f1c1d0faa
sporas <ianrong1371@gmail.com>
2021-02-04 17:51:04 -0800

    Update Startup.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,6 +45,8 @@ namespace TodoApi
                 app.UseDeveloperExceptionPage();
             }
 
+       //     app.UseHttpsRedirection();
+
             app.UseRouting();
 
             app.UseAuthorization();
